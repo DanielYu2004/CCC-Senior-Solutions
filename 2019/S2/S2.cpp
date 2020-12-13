2edf3dae6729e3857ceab18bd08c56932a15e1c1
Daniel Yu <55118464+DanielYu2004@users.noreply.github.com>
2020-12-13 16:53:19 -0600

    d

--- 2019/S2/S2.cpp
+++ 2019/S2/S2.cpp
@@ -1,9 +1,42 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+#include <cmath>
 
 using namespace std;
 
-int main(){
-    
+bool isPrime(int num)
+{
+    for (int a = 2; a <= sqrt(num); ++a)
+    {
+        if (num % a == 0)
+            return false;
+    }
 
-    return 0;
+    return true;
+}
+
+int main()
+{
+    int times;
+    cin >> times;
+
+    stringstream result;
+
+    for (int a = 0; a < times; ++a)
+    {
+        int num;
+        cin >> num;
+        num *= 2;
+
+        for (int b = 3; b <= num / 2; b += 2)
+        {
+            if (!isPrime(b) || !isPrime(num - b))
+                continue;
+
+            result << b << ' ' << num - b << '\n';
+            break;
+        }
+    }
+
+    cout << result.str();
 }
