ec8c43aec034da637d9e76bb264e37b28c636fe3
Daniel Yu <55118464+DanielYu2004@users.noreply.github.com>
2020-12-16 12:55:53 -0500

    d

---
+++ 2019/S3/S3.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include "template.h"
+
+using namespace std;
+
+
+int main(){
+    // vector<vector<int>> s;
+    // map<int, map<int, int>> m;
+
+    // for (int x = 0; x < 3; x++){
+    //     for (int y = 0; y < 3; y++){
+    //         string temp;
+    //         cin >> temp;
+    //         if (temp != "X"){
+    //             s[x][y] = stoi(temp);
+    //             m[x][y] = true;
+    //         } else{
+                
+    //         }
+    //     }
+    // }
+
+    // print_square(m);
+    print_vector();
+
+    return 0;
+}
