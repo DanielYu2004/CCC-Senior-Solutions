3f3d85e00b9e0c7d646dd8608475fb2ac6193e4d
Daniel Yu <55118464+DanielYu2004@users.noreply.github.com>
2021-02-04 20:04:55 -0500

    d

--- 2013/S4/main.cpp
+++ 2013/S4/main.cpp
@@ -19,7 +19,7 @@ typedef vector<vi> vvi;
 typedef vector<pii> vii; 
 typedef vector<LL> vl; 
 typedef vector<vl> vvl; 
-typedef set<int> si;
+typedef unordered_set<int> si;
 typedef set<si> sii;
 typedef vector<string> vs;
 
@@ -29,52 +29,39 @@ template<typename Container>
 void print_vector(Container v);
 void print_set(si s);
 
-si visited;
-si visitedd;
+vector<bool> visited(10000001);
+vector<bool> visitedd(10000001);
 string sol = "unknown";
 int startt, endd;
-map<int, si> taller, shorter;
+unordered_map<int, vi> taller, shorter;
 
-void recur(si nums){
-    si next_nums;
-    if (nums.size() > 0){
-        for (auto num: nums){
-            if (visited.find(num) == visited.end()){
-                visited.insert(num);
-                for (auto next: taller[num]){
-                    if (next == endd){
-                        visited.insert(next);
-                        sol = "yes";
-                        return;
-                    } else{
-                        next_nums.insert(next);
-                    }
+void recur(queue<int> nums){
+    while (!nums.empty()){
+        int num = nums.front();
+        nums.pop();
+        if (visited[num] == false){
+            visited[num] = true;
+            for (auto& next: taller[num]){
+                if (visited[next] == false){
+                    nums.push(next);
                 }
             }
         }
-        recur(next_nums);
     }
-    
 }
 
-void recurr(si nums){
-    si next_nums;
-    if (nums.size() > 0){
-         for (auto num: nums){
-            if (visitedd.find(num) == visitedd.end()){
-                visitedd.insert(num);
-                for (auto next: shorter[num]){
-                    if (next == endd){
-                        visitedd.insert(next);
-                        sol = "no";
-                        return;
-                    } else{
-                        next_nums.insert(next);
-                    }
+void recurr(queue<int> nums){
+    while (!nums.empty()){
+        int num = nums.front();
+        nums.pop();
+        if (visitedd[num] == false){
+            visitedd[num] = true;
+            for (auto& next: shorter[num]){
+                if (visitedd[next] == false){
+                    nums.push(next);
                 }
             }
         }
-        recurr(next_nums);
     }
    
 }
@@ -82,34 +69,35 @@ void recurr(si nums){
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
 
-    std::ifstream in("s4.6-1.in");
-    std::streambuf *cinbuf = std::cin.rdbuf();
-    std::cin.rdbuf(in.rdbuf()); 
-
     int n, m;
     cin >> n >> m;
-
 
     FOR(i, 0, m){
         int x, y;
         cin >> x >> y;
-        taller[x].insert(y);
-        shorter[y].insert(x);
+        taller[x].PB(y);
+        shorter[y].PB(x);
     }
 
     cin >> startt >> endd;
 
-    si temp;
-    temp.insert(startt);
+    queue<int> temp;
+    temp.push(startt);
 
     recur(temp);
+    if (visited[endd] == true){
+        sol = "yes";
+    } 
     if (sol != "yes"){
         recurr(temp);
     }
     
-
+    if (visitedd[endd] == true){
+        sol = "no";
+    }
+    
     cout << sol;
-
+    
     return 0;
 }
 
